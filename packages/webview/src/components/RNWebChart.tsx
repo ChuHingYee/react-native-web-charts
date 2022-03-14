08ceb5d73476531aa872479d78156dddcc9aae8c
chuhingyee <951034205@qq.com>
2022-03-14 12:26:52 +0800

    feat(webview): add webview loading tip

--- packages/webview/src/components/RNWebChart.tsx
+++ packages/webview/src/components/RNWebChart.tsx
@@ -1,4 +1,4 @@
-import React, {memo, forwardRef} from 'react';
+import React, {useState, memo, forwardRef} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {StyleProp, ViewStyle} from 'react-native';
 import {WebView} from 'react-native-webview';
@@ -7,23 +7,35 @@ import type {Ref} from 'react';
 
 interface WebChartProps
   extends Omit<WebViewProps, 'javaScriptEnabled' | 'style'> {
-  children?: JSX.Element | JSX.Element[] | string | any;
   containerStyle?: StyleProp<ViewStyle>;
+  webStyle?: StyleProp<ViewStyle>;
   emptyText?: string;
   isEmpty?: boolean;
-  webStyle?: StyleProp<ViewStyle>;
+  emptyComponent?: JSX.Element | JSX.Element[] | string | any;
+  hasLoadingComponent?: boolean;
+  loadingText?: string;
+  loadingComponent?: JSX.Element | JSX.Element[] | string | any;
 }
 
 const RNWebChart = (props: WebChartProps, ref?: Ref<WebView>) => {
   const {
     containerStyle,
     webStyle,
-    children,
+    emptyComponent,
     isEmpty,
     emptyText,
+    loadingComponent,
+    loadingText,
+    hasLoadingComponent,
     source,
+    onLoad,
     ...rest
   } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const handleOnLoad: WebViewProps['onLoad'] = event => {
+    setIsLoading(false);
+    onLoad && onLoad(event);
+  };
   return (
     <View style={styles.container} {...containerStyle}>
       <WebView
@@ -33,14 +45,24 @@ const RNWebChart = (props: WebChartProps, ref?: Ref<WebView>) => {
         javaScriptEnabled
         scrollEnabled={false}
         source={source}
+        onLoad={handleOnLoad}
         {...rest}
       />
       {isEmpty && (
-        <View style={styles.empty}>
-          {children ? (
-            children
+        <View style={styles.wrap}>
+          {emptyComponent ? (
+            emptyComponent
           ) : (
             <Text>{emptyText ? emptyText : '暂无数据'}</Text>
+          )}
+        </View>
+      )}
+      {isLoading && hasLoadingComponent && (
+        <View style={styles.wrap}>
+          {loadingComponent ? (
+            loadingComponent
+          ) : (
+            <Text>{loadingText ? loadingText : '正在加载'}</Text>
           )}
         </View>
       )}
@@ -52,7 +74,7 @@ const styles = StyleSheet.create({
   container: {
     height: 300,
   },
-  empty: {
+  wrap: {
     position: 'absolute',
     top: 0,
     left: 0,
