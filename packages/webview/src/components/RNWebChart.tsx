c819c6c6b6a872ac793b885ceb70ad3bef95cd5c
chuhingyee <chuhingyee@gmail.com>
2022-03-27 11:14:59 +0800

    fix(webview): fix the wrap show unnormal when in some backgroundColor

--- packages/webview/src/components/RNWebChart.tsx
+++ packages/webview/src/components/RNWebChart.tsx
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     zIndex: 2,
     alignItems: 'center',
     justifyContent: 'center',
